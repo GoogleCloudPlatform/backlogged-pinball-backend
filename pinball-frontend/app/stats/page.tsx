327a637f68e4a5888e95c5c72a97a7e58bfe4d29
Luke Schlangen <lukeschlangen@gmail.com>
2024-03-11 16:29:41 -0500

    feat: remove comment

--- pinball-frontend/app/stats/page.tsx
+++ pinball-frontend/app/stats/page.tsx
@@ -17,8 +17,6 @@ import AverageScore from "../components/average-score";
 import HighScore from "../components/high-score";
 import LongestGameLength from "../components/longest-game-length";
 
-
-// Stats page for pinball game
 export default function Stats() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
