9470cf495a3c7c55538b3104b0e58c16f39cd8b5
Luke Schlangen <lukeschlangen@gmail.com>
2024-04-04 21:09:32 -0500

    format landing page for mobile

--- pinball-frontend/app/page.tsx
+++ pinball-frontend/app/page.tsx
@@ -4,65 +4,53 @@ import QRCodeLink from "@/app/components/qr-code-link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <QRCodeLink url="https://goo.gle/backlogged" />
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-8 mb-32 grid text-center md:max-w-5xl md:w-full md:mb-0 grid-cols-2 md:grid-cols-4 md:text-left">
         <Link
           href="/stats"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-2 border-black px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 bg-white m-2"
         >
-          <h2 className={`mb-3 text-xl font-semibold`}>
-            Stats{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`md:mb-3 md:text-xl font-semibold`}>
+            Stats
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`hidden md:block m-0 max-w-[30ch] text-sm opacity-50`}>
             See Player Stats
           </p>
         </Link>
 
         <Link
           href="/events"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-2 border-black px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 bg-white m-2"
         >
-          <h2 className={`mb-3 text-xl font-semibold`}>
-            Event Stream{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`md:mb-3 md:text-xl font-semibold`}>
+            Event Stream
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`hidden md:block m-0 max-w-[30ch] text-sm opacity-50`}>
             View the live events as they happen on the pinball machine.
           </p>
         </Link>
 
         <Link
           href="/learn"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-2 border-black px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 bg-white m-2"
         >
-          <h2 className={`mb-3 text-xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`md:mb-3 md:text-xl font-semibold`}>
+            Learn
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`hidden md:block m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn to use the Google Cloud products used in this project.
           </p>
         </Link>
 
         <Link
           href="/about"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-2 border-black px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 bg-white m-2"
         >
-          <h2 className={`mb-3 text-xl font-semibold`}>
-            About{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`md:mb-3 md:text-xl font-semibold`}>
+            About
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`hidden md:block m-0 max-w-[30ch] text-sm opacity-50`}>
             See how this project was built.
           </p>
         </Link>
@@ -83,14 +71,14 @@ export default function Home() {
           </div>
         </div>
         <Image
-          className="fixed -z-30 top-80 motion-safe:animate-jump"
+          className="fixed -z-30 -bottom-12 motion-safe:animate-jump"
           src="/backlogged-beaver.png"
           width={500}
           height={500}
           alt="Cartoon beaver holding a keyboard"
         />
         <Image
-          className="fixed -z-50 top-80"
+          className="fixed -z-50 bottom-0 md:-bottom-96"
           src="/backlogged-background.png"
           sizes="100vw"
           style={{
