df2e5f93a5a7125db3ffaf6b2425cceffed16987
Mofi Rahman <mofi@google.com>
2024-09-26 13:44:29 -0400

    add kubernetes yaml and dockerfile for genai

---
+++ k8s/cluster.sh
@@ -0,0 +1,30 @@
+gcloud config set project backlogged-ai
+export PROJECT_ID=$(gcloud config get project)
+export REGION=us-west4
+export ZONE=us-west4-a
+export CLUSTER_NAME=gemma
+export HF_TOKEN=<YOUR HUGGING FACE TOKEN> 
+
+gcloud container clusters create ${CLUSTER_NAME} \
+  --project=${PROJECT_ID} \
+  --region=${REGION} \
+  --workload-pool=${PROJECT_ID}.svc.id.goog \
+  --release-channel=rapid   --num-nodes=1
+
+gcloud beta container --project=${PROJECT_ID} node-pools create "gpu" \
+  --cluster ${CLUSTER_NAME} \
+  --zone ${ZONE} \
+  --machine-type "a3-highgpu-8g" \
+  --accelerator "type=nvidia-h100-80gb,count=8,gpu-driver-version=latest" \
+  --spot --num-nodes "1"
+
+gcloud beta container --project=${PROJECT_ID} node-pools create "gpu" \
+  --cluster ${CLUSTER_NAME} \
+  --zone ${ZONE} \
+  --machine-type "e2-standard-4" \
+  --num-nodes "2"
+
+
+kubectl create secret generic hf-secret \
+--from-literal=hf_api_token=$HF_TOKEN \
+--dry-run=client -o yaml | kubectl apply -f -
