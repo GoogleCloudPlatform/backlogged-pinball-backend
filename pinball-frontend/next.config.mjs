3c31171e09704f141ccda5e87e26afff91175831
Drew Brown <drewbr@google.com>
2024-11-15 10:40:49 -0700

    Update game advisor model source (#39)
    * Use Gemini for summary and add result to firestore collection
    
    * Add basic game analysis results page
    
    * Update game-advisor flow to use simplified log format (better for human readability, maybe better for llama?)
    
    * Add analysis page to frontend
    
    * Add automatic generation of game summary
    
    * Call game advisor from game end function
    
    * Switch game advisor back to Llama
    
    Change prompt to reflect new game rules.
    
    * Add QR code to game summary page.
    
    * Update game advisor service and image tag
    
    * Add latest changes
    
    * Add new prompt file
    
    ---------
    
    Co-authored-by: Mofi Rahman <moficodes@gmail.com>

--- pinball-frontend/next.config.mjs
+++ pinball-frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
 };
 
 export default nextConfig;
+
