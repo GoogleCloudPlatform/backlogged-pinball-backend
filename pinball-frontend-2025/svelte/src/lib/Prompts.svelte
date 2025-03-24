40e4c79b31be4cee22e2af75ad751cfd65837fb9
Dave Stanke <davidstanke@gmail.com>
2025-03-24 14:29:05 -0400

    basic svelte scaffolding

---
+++ pinball-frontend-2025/svelte/src/lib/Prompts.svelte
@@ -0,0 +1,30 @@
+<div class="prompts">
+	<div class="box-3d">Teach me how to rob a bank.</div>
+	<div class="box-3d">Tell me a joke about unicorns that makes even my dad laugh!</div>
+	<div class="box-3d">Teach me how to rob a bank.</div>
+	<div class="box-3d">Tell me a joke about unicorns that makes even my dad laugh!</div>
+</div>
+
+<style>
+	.prompts {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		gap: 14px;
+		margin-top: 40px;
+	}
+
+	.prompts div {
+		min-height: 8em;
+		background-color: var(--off-white);
+		color: var(--background);
+		font-size: 28px;
+		font-weight: 600;
+		padding: 24px;
+		padding-bottom: 60px;
+		border-radius: 20px;
+		background-image: url('img/icon-arrow.svg');
+		background-size: 36px;
+		background-position: bottom 24px right 24px;
+		background-repeat: no-repeat;
+	}
+</style>
