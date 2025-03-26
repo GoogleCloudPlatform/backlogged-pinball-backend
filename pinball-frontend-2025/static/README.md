fa2474fa99fee604b4e963053f7e07df2a953a76
Dave Stanke <davidstanke@gmail.com>
2025-03-26 16:08:47 -0400

    Add static assets and initial svelte implementation (#56)
    * add initial html/css for 2025 frontend
    
    * move static implementation into its own folder
    
    * basic svelte scaffolding
    
    * add prompt box content

---
+++ pinball-frontend-2025/static/README.md
@@ -0,0 +1,3 @@
+# armorup-pinball-frontend
+
+## TODO: A cloud run service that serves these pages. On various clicks, it calls to the existing cloud run services.
---
+++ pinball-frontend-2025/svelte/src/routes/prompt-input/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Prompts from '$lib/Prompts.svelte';
+
+  let userPrompt = $state('');
+</script>
+
+<header>
+	<div class="cloud-logo"></div>
+	<div class="how-it-works">
+		<a class="text-3d box-3d" alt="how it works" href=".">How it works</a>
+	</div>
+</header>
+<main>
+	<h1 class="text-3d">Submit a prompt to test Model Armor</h1>
+	<form action="#" class="box-3d">
+		<input type="text" placeholder="What can I help you with?" id="promt-input-field" bind:value="{userPrompt}" />
+		<button class="arrow" aria-label="promt-input-field"></button>
+	</form>
+  <Prompts bind:userPrompt />
+
+</main>
+
+<svelte:head>
+	<style>
+		body {
+			background-image: url('img/bg-color.svg');
+		}
+	</style>
+</svelte:head>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px;
+		margin: 0 20px;
+	}
+
+	main {
+		width: 70%;
+		margin: 0 auto;
+	}
+
+	.how-it-works a {
+		display: block;
+		background-color: var(--google-blue);
+		color: var(--white);
+		font-size: 18px;
+		font-weight: 700;
+		border-radius: 50vh;
+		padding: 16px 32px;
+	}
+
+	.cloud-logo {
+		width: 50px;
+		height: 40px;
+		background-image: url('img/cloud-logo.svg');
+		background-size: contain;
+		background-position: center center;
+		background-repeat: no-repeat;
+	}
+</style>
